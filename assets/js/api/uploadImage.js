fe64ed9228ee80fe75c50cec96315c564a74bb17
Tvick22 <142449252+Tvick22@users.noreply.github.com>
2025-05-31 14:38:40 -0700

    upload image with title+desc

--- assets/js/api/uploadImage.js
+++ assets/js/api/uploadImage.js
@@ -3,6 +3,9 @@ import { pythonURI } from "./config.js";
 document.addEventListener("DOMContentLoaded", function () {
   const uploadButton = document.getElementById("upload-btn");
   const fileInput = document.getElementById("file-input");
+  const postTitle = document.getElementById("title-input");
+  const postDescription = document.getElementById("description-input");
+
   if (!uploadButton) {
     console.error("Error: Can not find upload button in DOM (id='upload-btn')");
     return;
@@ -11,28 +14,65 @@ document.addEventListener("DOMContentLoaded", function () {
     console.error("Error: Can not find file input in DOM (id='file-input')");
     return;
   }
+  if (!postTitle) {
+    console.error("Error: Can not find file input in DOM (id='title-input')");
+    return;
+  }
+  if (!postDescription) {
+    console.error(
+      "Error: Can not find file input in DOM (id='description-input')",
+    );
+    return;
+  }
 
   uploadButton.addEventListener("click", () => upload());
 });
 
 function upload() {
   const fileInput = document.getElementById("file-input");
+  const postTitle = document.getElementById("title-input").value;
+  const postDescription = document.getElementById("description-input").value;
   const file = fileInput.files[0];
 
-  const formData = new FormData();
-  formData.append("file", file);
-
-  fetch(`${pythonURI}/api/upload`, {
+  //create post
+  fetch(`${pythonURI}/api/post`, {
     method: "POST", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "default", // *default, no-cache, reload, force-cache, only-if-cached
     credentials: "include", // include, same-origin, omit
     headers: {
+      "Content-Type": "application/json",
       "X-Origin": "client", // New custom header to identify source
     },
-    body: formData,
+    body: JSON.stringify({
+      title: postTitle,
+      description: postDescription,
+    }),
   })
     .then((response) => response.json())
-    .then((result) => console.log("Success:", result))
+    .then((post) => {
+      //after making post, upload image.
+      const formData = new FormData();
+
+      console.log(post);
+      console.log(post.id);
+      formData.append("file", file);
+      formData.append("post_id", post.id);
+      formData.append("uid", post.user.id);
+
+      fetch(`${pythonURI}/api/upload`, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "default", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "include", // include, same-origin, omit
+        headers: {
+          "X-Origin": "client", // New custom header to identify source
+        },
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((result) => console.log("Success:", result))
+        .catch((error) => console.error("Error:", error));
+    })
     .catch((error) => console.error("Error:", error));
 }
