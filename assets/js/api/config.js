cb4f296c27002e6b8edc85de370e90bf49ae20f1
Tvick22 <142449252+Tvick22@users.noreply.github.com>
2025-06-05 09:37:56 -0700

    local port

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,8 +1,8 @@
 export var pythonURI;
 if (location.hostname === "localhost") {
-  pythonURI = "http://localhost:8104";
+  pythonURI = "http://localhost:4200";
 } else if (location.hostname === "127.0.0.1") {
-  pythonURI = "http://127.0.0.1:8104";
+  pythonURI = "http://127.0.0.1:4200";
 } else {
   pythonURI = "https://shotspot.opencodingsociety.com/";
 }
